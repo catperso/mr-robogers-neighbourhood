b3132810d7ef5a783d91096d495c7d44bb779f31
Matt Casperson <mc.casperson@gmail.com>
2021-10-29 11:36:13 -0700

    refactor roboger() to pass 7th spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 // Business Logic
 
-function roboger(input) {
+function roboger(input, name) {
   if (input < 1) {
     return [0];
   }
 
   let targetNumber = parseInt(input);
   let numberArray = [];
+  let neighbour = '?';
+
+  if (name) {
+    neighbour = ", " + name + "?";
+  }
 
   for (let i=0; i <= targetNumber; i++) {
     numberArray.push(i);
@@ -14,7 +19,7 @@ function roboger(input) {
 
   numberArray = numberArray.map(function(number) {
     if (number.toString().includes(3)) {
-      return "Won't you be my neighbour?";
+      return "Won't you be my neighbour" + neighbour;
     } else if (number.toString().includes(2)) {
       return "Boop!";
     } else if (number.toString().includes(1)) {
@@ -34,7 +39,8 @@ $(document).ready(function() {
     event.preventDefault();
 
     const userInput = $("#user-number").val();
-    let outputArray = roboger(userInput);
+    const userName = $("#user-name").val().toUpperCase();
+    let outputArray = roboger(userInput, userName);
 
     $("ul").empty();
 

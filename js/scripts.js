bcdd8755767bfb7deb28a02b4ffd99821e87910a
Matt Casperson <mc.casperson@gmail.com>
2021-10-29 09:21:06 -0700

    refactor roboger() to pass fourth spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 // Business Logic
 
-function roboger(targetNumber) {
-  if (!parseInt(targetNumber)) {
+function roboger(input) {
+  if (!parseInt(input)) {
     return "Numbers only, please";
   }
-  if (targetNumber < 1) {
+  if (input < 1) {
     return 0;
   }
 
+  let targetNumber = parseInt(input);
   let numberArray = [];
 
   for (let i=0; i <= targetNumber; i++) {
     numberArray.push(i);
   }
+
+  numberArray = numberArray.map(function(number) {
+    if (number.toString().includes(3)) {
+      return "Won't you be my neighbour?";
+    } else {
+      return number;
+    }
+  });
 
   return numberArray;
 }

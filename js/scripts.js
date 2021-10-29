6ce667301f741008395c24a24c8c353fc7b68503
Matt Casperson <mc.casperson@gmail.com>
2021-10-29 08:57:09 -0700

    refactor roboger() to pass second spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@
 function roboger(targetNumber) {
   if (!parseInt(targetNumber)) {
     return "Numbers only, please";
+  } else if (targetNumber < 1) {
+    return 0;
   } else {
     return targetNumber;
   }
